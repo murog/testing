88a2f432c791e61b62031019d5f4055ce63754a2
Guillermina Muro <guillermina.muro@gmail.com>
2020-03-19 17:13:01 -0700

    updated acm branch

--- env-setup/config-management/install-config-sync.sh
+++ env-setup/config-management/install-config-sync.sh
@@ -5,8 +5,6 @@ export CLUSTER_NAME=$(kubectl config current-context)
 export BASE_DIR=${BASE_DIR:="${PWD}/.."}
 echo "BASE_DIR set to $BASE_DIR"
 
-source $BASE_DIR/config-management/config-repo-url.env
-
 echo "### "
 echo "### Begin install config sync"
 echo "### "
